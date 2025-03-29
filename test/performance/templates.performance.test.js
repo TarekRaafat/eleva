62f7d10399c4b5fd52352bcc6dddb84f4165652b
TarekRaafat <tarek.m.raafat@gmail.com>
2025-03-29 17:25:48 +0200

    Increase performance threshold for template parsing test from 25ms to 50ms

--- test/performance/templates.performance.test.js
+++ test/performance/templates.performance.test.js
@@ -68,6 +68,6 @@ describe("TemplateEngine Performance", () => {
     console.log(`Template parsing time: ${(end - start).toFixed(2)}ms`);
 
     // Performance assertion: parsing should be fast
-    expect(end - start).toBeLessThan(25); // Under 25ms
+    expect(end - start).toBeLessThan(50); // Under 50ms
   });
 });
